6df05cc005412cb1f7e1fd53a667bae5dc634f2d
unikevde <155786086+unikevde@users.noreply.github.com>
2024-01-31 19:21:05 -0500

    Update ActualizarEliminar.xaml.cs

--- Vistas/ActualizarEliminar.xaml.cs
+++ Vistas/ActualizarEliminar.xaml.cs
@@ -15,11 +15,18 @@ public partial class ActualizarEliminar : ContentPage
 
     private void btnEliminar_Clicked(object sender, EventArgs e)
     {
-
+	 string id=txtCodigo.Text.ToString();
+ 	string url = $"http://172.24.13.67/semana6/post.php?codigo={id}";
+ 	var content = await cliente.DeleteAsync(url);
+	 await Navigation.PushAsync(new Inicio());
     }
 
     private void btnActualizar_Clicked(object sender, EventArgs e)
     {
-
+	 string id = txtCodigo.Text.ToString();
+ 	string edad=txtNombre.Text.ToString();
+ 	string url = $"http://172.24.13.67/semana6/post.php";
+	var content = await cliente.PutAsync(url);
+        await Navigation.PushAsync(new Inicio());
     }
 }
