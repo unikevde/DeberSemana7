89ba4eddcd9c003a09b3fb96399ba692da207580
unikevde <155786086+unikevde@users.noreply.github.com>
2024-02-02 18:57:05 -0500

    Update ActualizarEliminar.xaml.cs

--- Vistas/ActualizarEliminar.xaml.cs
+++ Vistas/ActualizarEliminar.xaml.cs
@@ -24,7 +24,16 @@ public partial class ActualizarEliminar : ContentPage
     private void btnActualizar_Clicked(object sender, EventArgs e)
     {
 	 string id = txtCodigo.Text.ToString();
- 	string edad=txtNombre.Text.ToString();
+ 	 string nombre=txtNombre.Text.ToString();
+   	 string apellido=txtApellido.Text.ToString();
+         string edad= txtEdad.Text.ToString();
+
+         string url = "$"http://172.24.13.67/semana6/post.php?codigo=" + codigo + "&nombre=" + nombre + "&apellido=" + apellido + "&edad=" + edad;
+         WebClient cliente = new WebClient();
+         var parametros = new System.Collections.Specialized.NameValueCollection();
+         cliente.UploadValues(url, "PUT", parametros);
+         Navigation.PushAsync(new Views.Inicio(""));
+  
  	string url = $"http://172.24.13.67/semana6/post.php";
 	var content = await cliente.PutAsync(url);
         await Navigation.PushAsync(new Inicio());
